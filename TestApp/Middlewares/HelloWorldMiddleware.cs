6b4eb9d9650326d24e3d08ecd5f0dd3a243a94e0
Palienko.AN <Palienko.AN@dns-shop.ru>
2022-08-24 18:55:03 +1000

    Initial commit

---
+++ TestApp/Middlewares/HelloWorldMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace TestApp.Middlewares
+{
+    public class HelloWorldMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HelloWorldMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("HelloWorld");
+            await _next(context);
+        }
+    }
+
+    public static class AuthorizationAppBuilderExtensions
+    {
+        public static IApplicationBuilder UseHelloWorld(this IApplicationBuilder app)
+        {
+
+            return app.UseMiddleware<HelloWorldMiddleware>();
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TestApp.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Hello World");
+    await next(context);
+});
+
+app.UseHelloWorld();
+
+app.Run();
